fdc332309d3203f4ee7205d9b429bb69ecd52a63
praveen4developer <150101671+praveen4developer@users.noreply.github.com>
2023-11-08 16:45:07 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+function a() {
+    var elt = document.getElementsByClassName('dark-mode');
+    elt[0].classList.add('active')
+}
+function b() {
+    var elt = document.getElementsByClassName('dark-mode');
+    elt[0].classList.remove('active')
+}
+function openCity(cityName, elmnt, color) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    tablinks = document.getElementsByClassName("tablink");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].style.backgroundColor = "";
+    }
+
+    document.getElementById(cityName).style.display = "block";
+
+    elmnt.style.backgroundColor = color;
+}
+
+document.getElementById("defaultOpen").click();
